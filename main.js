6dad7a1357d0d16b019c6ee24a1c480e1e34c27e
JulianMorales1 <98353349+JulianMorales1@users.noreply.github.com>
2022-03-06 16:52:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,58 @@
 // Your code here.
+function getFirstName(objectFirstName){
+  let nameFirst = objectFirstName.firstName
 
+  return nameFirst
+}  
+
+function getLastName(objectLastName){
+  let nameLast = objectLastName.lastName
+
+  return nameLast
+}
+
+function getFullName(objectName){
+  let nameFull = objectName.firstName + " " + objectName.lastName
+
+  return nameFull
+}
+
+function setFirstName(objectChange, objectRename){
+
+  objectChange.firstName = objectRename
+  
+  return objectRename
+}
+
+function setAge(objectAge, objectNewAge){
+  objectAge.age = objectNewAge
+
+  return objectNewAge
+}
+
+function giveBirthday(birthdayAge){
+
+  if(birthdayAge.age === undefined){
+    birthdayAge.age = 1
+  }
+  else {
+    birthdayAge.age = birthdayAge.age + 1
+  }
+}
+
+function marry(spouseOne, spouseTwo){
+  spouseOne.married = true
+  spouseTwo.married = true
+  spouseOne.spouseName = spouseTwo.firstName + " " + spouseTwo.lastName
+  spouseTwo.spouseName = spouseOne.firstName + " " + spouseOne.lastName
+}
+
+function divorce(spouseOne,spouseTwo){
+  spouseOne.married = false
+  spouseTwo.married = false
+  delete spouseOne.spouseName
+  delete spouseTwo.spouseName 
+}
 
 
 
